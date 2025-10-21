a68d5bb5ca24f82ad0b1171304cab1e3fc175c49
jramma <jrmasip097@gmail.com>
2025-10-21 19:58:14 +0200

    feat: refactor header and footer components; implement standalone components and update styles

--- src/app/app.component.ts
+++ src/app/app.component.ts
@@ -1,11 +1,12 @@
 import { Component } from '@angular/core';
 import { RouterLink, RouterOutlet } from '@angular/router';
-import { FooterComponent } from './features/products/components/footer.component';
+import { FooterComponent } from './features/products/components/footer/footer.component';
+import { HeaderComponent } from './features/products/components/header/header.component';
 
 @Component({
   selector: 'app-root',
   standalone: true,
-  imports: [RouterOutlet, RouterLink, FooterComponent],
+  imports: [RouterOutlet, RouterLink, FooterComponent, HeaderComponent],
   templateUrl: './app.component.html',
   styleUrls: ['./app.component.scss'],
 })
